73a094351ecb79d6499d9f673abddb8bcd8dcd58
HE Joseph <joseph.he@axa.fr>
2020-02-11 12:32:40 +0100

    count any symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
@@ -12,27 +14,25 @@ public class AnalyticsCounter {
 	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]){
+		
+		HashMap<String,Integer> counter = new HashMap<String,Integer>();
+		
 		try {
 		// first get input
 			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 			String line = reader.readLine();
 	
-			int i = 0;	// set i to 0
 			int headCount = 0;	// counts headaches
 			while (line != null) {
-				i++;	// increment i
 				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headCount++;
-					System.out.println("number of headaches: " + headCount);
+				
+				if(counter.containsKey(line)) {
+					int value = counter.get(line)+1;
+					counter.put(line, value);
+				}else {
+					counter.put(line, 1);
 				}
-				else if (line.equals("rush")) {
-					rashCount++;
-				}
-				else if (line.contains("pupils")) {
-					pupilCount++;
-				}
-	
+				
 				line = reader.readLine();	// get another symptom
 			}
 			reader.close();
@@ -45,9 +45,9 @@ public class AnalyticsCounter {
 		try {
 			// next generate output
 			FileWriter writer = new FileWriter ("result.out");
-			writer.write("headache: " + headacheCount + "\n");
-			writer.write("rash: " + rashCount + "\n");
-			writer.write("dialated pupils: " + pupilCount + "\n");
+			for(Map.Entry<String, Integer> entry : counter.entrySet()) {
+				writer.write(entry.getKey()+": " + entry.getValue() + "\n");
+			}
 			writer.close();
 		}catch(IOException ioe) {
 			System.out.println("cannot write output file");
